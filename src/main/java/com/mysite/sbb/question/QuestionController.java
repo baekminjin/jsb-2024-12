56fade13dbbcfb6d1f1666c8977e5479716bf82e
baekminjin <bhs3392@naver.com>
2024-12-11 16:52:04 +0900

    question write page

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @Controller
@@ -35,5 +37,10 @@ public class QuestionController {
 		return "question_form";
 	}
 
+	@PostMapping("/create")
+	public String questionCreate(@RequestParam(value="subject") String subject, @RequestParam(value="content") String content) {
+		// TODO 질문을 저장한다.
+		return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
+	}
 
 }
